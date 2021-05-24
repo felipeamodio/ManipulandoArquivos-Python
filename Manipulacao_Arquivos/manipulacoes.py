40d5c405308abfc0ff08983b19acf534809bff62
Felipe <fealves.amodio@gmail.com>
2021-05-24 20:05:45 -0300

    adicionando mais arquivos

---
+++ Manipulacao_Arquivos/manipulacoes.py
@@ -0,0 +1,7 @@
+conteudo = "Estou testando criar um arquivo de texto. Então, estou textando?"
+
+arquivo = open("/Users/felipeamodio/PycharmProjects/Cap9/Manipulacao_Arquivos/novo_arquivo2.txt", "w")
+
+arquivo.write(conteudo)
+
+arquivo.close()
